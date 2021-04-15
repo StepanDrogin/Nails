d381cb681bc55e902e04f674e5176ccc9f1291ab
Stepan Drogin <drogenko.stepan@yandex.ru>
2021-04-15 23:52:22 +0300

    added slider,jquery,"about us" and slider

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 5,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
