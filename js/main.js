6c456ca7c30750835c2be9a664ef3918e0bfb009
Stepan Drogin <drogenko.stepan@yandex.ru>
2021-04-26 18:19:05 +0300

    added burger menu and some media queries

--- js/main.js
+++ js/main.js
@@ -24,3 +24,10 @@ $('.center').slick({
       }
     ]
   });
+
+  $(document).ready(function() {
+    $('.menu-burger__header').click(function() {
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+    });
+});
